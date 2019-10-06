955246fa2db224bfe02e662debfe695c342f8905
Samuel Cruz <45038543+Saam97@users.noreply.github.com>
2019-10-06 12:02:05 -0300

    Exemplo Interface Grafica

---
+++ Exercícios/ExemploGUI/src/exemplogui/ExemploGUI.java
@@ -0,0 +1,11 @@
+package exemplogui;
+
+public class ExemploGUI {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaExemplo te = new TelaExemplo();
+        te.setVisible(true);
+    }
+
+}
